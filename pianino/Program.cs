49f0d09e7c999469d5bae02e69dfdb443a301c46
kon1ac <144265321+kon1ac@users.noreply.github.com>
2023-10-30 13:29:23 +0300

    Update Program.cs

--- pianino/Program.cs
+++ pianino/Program.cs
@@ -1,82 +1,77 @@
-using System; 
-using System.Linq; 
-using System.Media; 
-using System.Runtime.InteropServices; 
-using System.Threading; 
-  
-public class Piano 
-{ 
-    private int[] firstOctave = new int[] { 262, 294, 330, 349, 392, 440, 494 }; 
-    private int[] secondOctave = new int[] { 523, 587, 659, 698, 784, 880, 988 }; 
-    private int[] currentOctave; 
-  
-    public Piano() 
-    { 
-        currentOctave = firstOctave;  
-    } 
-  
-    private void PlaySound(int frequency, int duration) 
-    { 
-        Console.Beep(frequency, duration); 
-    } 
-  
-    private int[] ChangeOctave(int octave) 
-    { 
-        switch (octave) 
-        { 
-            case 2: 
-                return secondOctave; 
-            default: 
-                return firstOctave; 
-        } 
-    } 
-  
-    public void Start() 
-    { 
-        int octave = 1; 
-        Console.WriteLine("Для смены октавы используйте клавиши F1, F2, F3 и т.п."); 
-  
-        while (true) 
-        { 
-            if (Console.KeyAvailable) 
-            { 
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true); 
-  
-                if (keyInfo.Key == ConsoleKey.F1) 
-                { 
-                    octave = 1; 
-                    currentOctave = ChangeOctave(octave); 
-                } 
-                else if (keyInfo.Key == ConsoleKey.F2) 
-                { 
-                    octave = 2; 
-                    currentOctave = ChangeOctave(octave); 
-                } 
-                else if (keyInfo.Key == ConsoleKey.Escape) 
-                { 
-                    break; 
-                } 
-                else 
-                { 
-                    int keyNoteIndex = Array.IndexOf(currentOctave, (int)keyInfo.KeyChar); 
-  
-                    if (keyNoteIndex != -1) 
-                    { 
-                        PlaySound(currentOctave[keyNoteIndex], 200); 
-                    } 
-                } 
-            } 
-  
-            Thread.Sleep(10); 
-        } 
-    } 
-} 
-  
-public class Program 
-{ 
-    static void Main(string[] args) 
-    { 
-        Piano piano = new Piano(); 
-        piano.Start(); 
-    } 
+using System;
+
+class Program
+{
+    static int[][] octaves = new int[][] {
+        new int[] { 200, 300, 400, 500, 600 },
+        new int[] { 700, 800, 900, 1000, 1100 },
+        new int[] { 1200, 1300, 1400, 1500, 1600 }
+    };
+
+    static int currentOctave = 0;
+
+    static void PlaySound(int frequency, int duration)
+    {
+        Console.Beep(frequency, duration);
+    }
+
+    static int[] ChangeOctave(int octave)
+    {
+        if (octave >= 0 && octave < octaves.Length)
+        {
+            currentOctave = octave;
+            return octaves[octave];
+        }
+        else
+        {
+            currentOctave = 0;
+            return octaves[0];
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            var key = Console.ReadKey().Key;
+
+            if (key >= ConsoleKey.F1 && key <= ConsoleKey.F3)
+            {
+                int octave = (int)key - (int)ConsoleKey.F1;
+                ChangeOctave(octave);
+                continue;
+            }
+
+            int frequency;
+            int duration;
+
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    frequency = ChangeOctave(currentOctave)[0];
+                    duration = 500;
+                    break;
+                case ConsoleKey.S:
+                    frequency = ChangeOctave(currentOctave)[1];
+                    duration = 500;
+                    break;
+                case ConsoleKey.D:
+                    frequency = ChangeOctave(currentOctave)[2];
+                    duration = 500;
+                    break;
+                case ConsoleKey.F:
+                    frequency = ChangeOctave(currentOctave)[3];
+                    duration = 500;
+                    break;
+                case ConsoleKey.W:
+                    frequency = ChangeOctave(currentOctave)[4];
+                    duration = 400;
+                    break;
+                default:
+                    continue;
+            }
+
+            PlaySound(frequency, duration);
+        }
+    }
 }
